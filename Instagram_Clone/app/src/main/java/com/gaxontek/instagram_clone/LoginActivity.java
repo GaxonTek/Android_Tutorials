ed166ca0c5a7b1a48f3befe75aff70dd21d77fba
GaxonTek <gaxontek@gmail.com>
2020-10-29 19:41:58 -0600

    Setting Up Your Development Environment
    Tut 1

---
+++ Instagram_Clone/app/src/main/java/com/gaxontek/instagram_clone/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.gaxontek.instagram_clone;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private  FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(this);
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+}
