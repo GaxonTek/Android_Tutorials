4fed0d2cff695e3a90b5675e5c7272496f1416c7
GaxonTek <gaxontek@gmail.com>
2020-12-07 12:02:26 -0700

    Tutorial 3 finished

--- Instagram_Clone/app/src/main/java/com/gaxontek/instagram_clone/authentication/LoginActivity.java
+++ Instagram_Clone/app/src/main/java/com/gaxontek/instagram_clone/authentication/LoginActivity.java
@@ -1,20 +1,46 @@
 package com.gaxontek.instagram_clone.authentication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.gaxontek.instagram_clone.R;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.auth.FirebaseAuth;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.gaxontek.instagram_clone.R;
+import com.gaxontek.instagram_clone.main_navigation.MainNavigationActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String TAG = "LoginActivity";
+
     private  FirebaseAuth mAuth;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference myRef;
+
+    //Facebook Login
+    LoginButton loginButton;
+    CallbackManager mCallbackManager;
 
     private Button btnSignIn;
     private TextView tvNewAccount;
@@ -23,8 +49,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         FirebaseApp.initializeApp(this);
         mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance();
+        myRef = mDatabase.getReference("users");
 
         UtilsInitialization();
 
@@ -42,11 +71,79 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        //Facebook Login
+        mCallbackManager = CallbackManager.Factory.create();
+        loginButton.setPermissions("email", "public_profile");
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
 
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+                // ...
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "facebook:onError", error);
+                // ...
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            Log.i(TAG, "onStart: Someone logged in <3");
+            Intent intent = new Intent(LoginActivity.this, MainNavigationActivity.class);
+            startActivity(intent);
+        } else {
+            Log.i(TAG, "onStart: No one logged in :/");
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Pass the activity result back to the Facebook SDK
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     private void UtilsInitialization(){
         btnSignIn = findViewById(R.id.btn_signin);
         tvNewAccount = findViewById(R.id.text_create_account);
+        loginButton = findViewById(R.id.login_button);
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            final User mUser = new User(user.getDisplayName(), user.getEmail());
+                            myRef.child(user.getUid()).setValue(mUser);
+                            Intent intent = new Intent(LoginActivity.this, MainNavigationActivity.class);
+                            startActivity(intent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
